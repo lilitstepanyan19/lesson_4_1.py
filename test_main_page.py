576cc7ea9c34c20780ac047def9a94b5ba0fb52f
lilitstepanyan19 <lil-lid@mail.ru>
2022-11-18 19:53:49 +0400

    update all

--- test_main_page.py
+++ test_main_page.py
@@ -1,10 +1,20 @@
 from pages.main_page import MainPage
+from pages.login_page import LoginPage
 
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
-    page.open
-    page.go_to_login_page()
-    page.should_by_login_link()
-    
+    # link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
+    # page = MainPage(browser, link)
+    # page.open
+    # page.go_to_login_page()
+
+# def test_guest_should_see_login_page(browser):
+#     link = "http://selenium1py.pythonanywhere.com/"
+#     # link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
+#     page = MainPage(browser, link)
+#     page.open
+    # page.should_by_login_link()
+    login = LoginPage(browser, link)
+    login.open
+    login.should_be_login_form()
